0e3be0f9cbedc5f5539094bdb6e0c279c3b462c3
tonywaouw <tonywaouw@gmail.com>
2013-04-22 17:27:24 +0200

    Ajout de Meal

--- Mister Spoon/src/spoon/misterspoon/Meal.java
+++ Mister Spoon/src/spoon/misterspoon/Meal.java
@@ -1,15 +1,40 @@
 package spoon.misterspoon;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
 public class Meal {
 
+	public MySQLiteHelper sqliteHelper;
+	
 	private String mealname;
 	private double mealprice;
-	private int stock; // On les sort d'ou ?
-	private String description; // On les sort d'ou ?
+	private int stock; 
+	private String description;
 	
-	public Meal(String mealname, double mealprice) {
-        this.mealname = mealname;
-        this.mealprice = mealprice;
+	public Meal(String mealname, MySQLiteHelper sqliteHelper) { 
+		this.mealname = mealname;
+        this.sqliteHelper = sqliteHelper;
+		SQLiteDatabase db = sqliteHelper.getReadableDatabase();
+		
+		// Select the price
+		Cursor cursor = db.rawQuery("SELECT " + MySQLiteHelper.Meal_column[3] + " FROM " + MySQLiteHelper.TABLE_Meal + " WHERE " + MySQLiteHelper.Meal_column[1] + "=" + mealname, null);
+		if (cursor.moveToFirst()) {//If the information exists
+			this.mealprice = cursor.getDouble(0);
+		}
+		
+		// Check stock
+		cursor = db.rawQuery("SELECT " + MySQLiteHelper.Meal_column[4] + " FROM " + MySQLiteHelper.TABLE_Meal + " WHERE " + MySQLiteHelper.Meal_column[1] + "=" + mealname, null);
+		if (cursor.moveToFirst()) {//If the information exists
+			this.stock = cursor.getInt(0);
+		}
+		
+		// Description
+		cursor = db.rawQuery("SELECT " + MySQLiteHelper.Meal_column[5] + " FROM " + MySQLiteHelper.TABLE_Meal + " WHERE " + MySQLiteHelper.Meal_column[1] + "=" + mealname, null);
+		if (cursor.moveToFirst()) {//If the information exists
+			this.description = cursor.getString(0);
+		}
+		
 	}
 	
 	public String getMealName() {
@@ -17,14 +42,15 @@ public class Meal {
 	}
 	
 	public int getMealStock() {
-		return stock; // On les sort d'ou ?
+		return this.stock; 
 	}
 	
 	public String getMealDescription() {
-		return this.description; // On les sort d'ou ?
+		return this.description; 
 	}
 	
 	public double getMealPrice() {
 		return this.mealprice;
 	}
+
 }
