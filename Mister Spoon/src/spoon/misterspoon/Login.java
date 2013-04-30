2ce1abed8c3f76c6881d63892fa88ffcf28de15a
tonywaouw <tonywaouw@gmail.com>
2013-04-30 09:32:57 +0200

    Ajout Theme Dark/Light Modif des icones de la carte

--- Mister Spoon/src/spoon/misterspoon/Login.java
+++ Mister Spoon/src/spoon/misterspoon/Login.java
@@ -3,13 +3,24 @@ package spoon.misterspoon;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class Login extends Activity {
+	
+	Button themeD = null;
+	Button themeL = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+		
+		themeD = (Button)findViewById(R.id.activity_login_dark);
+		themeL = (Button)findViewById(R.id.activity_login_light);
+		themeD.setOnClickListener(themeDListener);
+		themeL.setOnClickListener(themeLListener);
 	}
 
 	@Override
@@ -18,5 +29,20 @@ public class Login extends Activity {
 		getMenuInflater().inflate(R.menu.login, menu);
 		return true;
 	}
+	
+	
+	//C'est la partie pour le changement de theme
+	
+	private OnClickListener themeLListener = new OnClickListener() { @Override
+		public void onClick(View v) {
+		Utils.changeToTheme(Login.this, Utils.THEME_DEFAULT); 
+	}
+	};
+
+	private OnClickListener themeDListener = new OnClickListener() { @Override
+		public void onClick(View v) {
+		Utils.changeToTheme(Login.this, Utils.THEME_DARK); 
+	}
+	};
 
 }
