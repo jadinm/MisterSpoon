496b2e5292397722133b40b1751d38dad4dfc785
tonywaouw <tonywaouw@gmail.com>
2013-05-05 15:19:48 +0200

    Update mealbuilder

--- Mister Spoon/src/spoon/misterspoon/Meal_BuilderActivity.java
+++ Mister Spoon/src/spoon/misterspoon/Meal_BuilderActivity.java
@@ -15,15 +15,15 @@ import android.widget.Toast;
 public class Meal_BuilderActivity extends Activity {
 	
 	private MySQLiteHelper sqliteHelper;
-	private String mealName;
 	private String restName;
 	
 	Context context = this;
 	MealBuilder mealB; 
-	Meal meal;//lien vers le plat courant.
+	Meal meal;
 	EditText mealPrice = null;
 	EditText mealStock = null;
 	EditText mealDescription = null;
+	EditText mealName = null;
 	Button update = null;
 	ImageView mealImage = null;
 	
@@ -43,19 +43,22 @@ public class Meal_BuilderActivity extends Activity {
 		
 		
 		//We take the informations about the meal viewed
-		mealName = i.getStringExtra(Carte.mealName);//TODO
-		restName = i.getStringExtra(Profil_Restaurant.restName);
+		String emailPerso = i.getStringExtra(Login.email);
 		
 		//We create the object Meal associated with this mealName and all his informations
 		sqliteHelper= new MySQLiteHelper(this);
-		RestaurantOwner r = new RestaurantOwner(sqliteHelper , emailResto);
+		RestaurantOwner r = new RestaurantOwner(sqliteHelper , emailPerso);
 		mealB = new MealBuilder (sqliteHelper, r);
-		meal = new Meal(mealName, restName, sqliteHelper);
 		Log.v("start",emailResto);
 		
 		
-		
-		
+
+		mealName = (EditText) findViewById(R.id.meal_builder_name);
+
+
+		if (""+meal.getMealName()!=null) {
+			mealName.setText(meal.getMealName());
+		}		
 
 		//We can now define all the widgets
 		mealImage = (ImageView) findViewById(R.id.meal_imageview);
@@ -63,17 +66,10 @@ public class Meal_BuilderActivity extends Activity {
 		//TODO mettre images
 		
         
-        
-        
 
-        //Faudrait mettre un champ pour le nom du plat...(name) a moins qu'il soit deja connu ?
-        
-        
 		mealPrice = (EditText) findViewById(R.id.meal_builder_price);
 		mealStock = (EditText) findViewById(R.id.meal_builder_stock);
 		mealDescription = (EditText) findViewById(R.id.meal_builder_description);
-		
-		
 		if (""+meal.getMealPrice(true)!=null) {
 			mealPrice.setText(""+meal.getMealPrice(true));
 		}		
@@ -83,6 +79,8 @@ public class Meal_BuilderActivity extends Activity {
 		if (meal.getMealDescription(true)!=null) {
 			mealDescription.setText(meal.getMealDescription(true));
 		}
+
+		meal = new Meal(mealName.getText().toString(), restName, sqliteHelper);
 		
 		update = (Button) findViewById(R.id.meal_update);
 		update.setOnClickListener(new View.OnClickListener() {//Update the informations
@@ -90,25 +88,28 @@ public class Meal_BuilderActivity extends Activity {
 			public void onClick(View v) {
 				
 
-				int value = Meal.isInDatabase(sqliteHelper, (String) meal.getMealName(), restName);
-				if(value==2 && meal.getMealName().equals(name.getText().toString())) {//If it already exists
-					name.setText(meal.getMealName());
-					Toast toast = Toast.makeText(context, name.getText().toString() + getString(R.string.meal_builder_toast_already_exist), Toast.LENGTH_SHORT);
+				int value = meal.isInDatabase(sqliteHelper, (String) meal.getMealName(), restName);
+				if(value==2 && meal.getMealName().equals(mealName.getText().toString())) {//If it already exists
+					mealName.setText(meal.getMealName());
+					Toast toast = Toast.makeText(context, mealName.getText().toString() + getString(R.string.meal_builder_toast_already_exist), Toast.LENGTH_SHORT);
 					toast.show();
 					
 					return;
 				}
-				meal.setName(name.getText().toString());
+				
+				if (mealName.getText().toString().length()>0) {
+					mealB.setMealName(mealName.getText().toString());
+				}
 				
 				if (mealPrice.getText().toString().length()>0) {
-					mealB.setMealPrice(Integer.parseInt(mealPrice.getText().toString()));//TODO
+					mealB.setMealPrice(Integer.parseInt(mealPrice.getText().toString()));
 				}
 				
 				if (mealDescription.getText().toString().length()>0) {
-					mealB.setMealDescription(mealDescription.getText().toString());//TODO
+					mealB.setMealDescription(mealDescription.getText().toString());
 				}
 				if (mealStock.getText().toString().length()>0) {
-					mealB.setMealStock(Integer.parseInt(mealStock.getText().toString()));//TODO
+					mealB.setMealStock(Integer.parseInt(mealStock.getText().toString()));
 				}
 				
 				Toast toasted = Toast.makeText(context, getString(R.string.meal_builder_toast_uptodate), Toast.LENGTH_SHORT);
